8df89c2f89aea509984fb3d6049680e74e744643
mbeganovic3 <merisb@ad.unc.edu>
2020-01-24 19:04:26 -0500

    A2

--- Assets/TreasureHunter.cs
+++ Assets/TreasureHunter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TreasureHunter : MonoBehaviour
 {
+    
     public List<CollectibleTreasure> collectiblesInScene; 
     public TreasureHunterInventory inventory;
     public OVRCameraRig oVRCameraRig;
